c5bdfcd2c92dd9d61927eb183bdf0322159db06e
sammy <sammyoina@gmail.com>
2022-10-19 14:01:52 +0300

    rrtr

--- include/confs.h
+++ include/confs.h
@@ -1,27 +1,39 @@
 #ifndef CONNFS_h
 #define CONNFS_h
 #include <FCH.h>
-#define MOTOR_BASE_SPEED_LEFT 80
-#define MOTOR_BASE_SPEED_RIGHT 104
+#define MOTOR_BASE_SPEED_LEFT 98
+#define MOTOR_BASE_SPEED_RIGHT 97
+FCH driverLeft(2);
+FCH driverRight(16);
 
 int line_position;
 
-void Drive60LeftRoutine(FCH leftDriver, FCH rightDriver){
-    leftDriver.setSpeed(104);
-    rightDriver.setSpeed(20);
-    delay(20);
-    leftDriver.setSpeed(104);
-    rightDriver.setSpeed(80);
-    delay(20);
-    leftDriver.brake();
+void Drive90LeftRoutine(void){
+    driverLeft.setSpeed(100);
+    driverRight.setSpeed(90);
+    delay(500);
+    driverLeft.brake();
+    driverRight.brake();
+    delay(1000);
+    driverLeft.setSpeed(101);
+    driverRight.setSpeed(100);
+    delay(500);
+    driverLeft.brake();
+    driverRight.brake();
+    delay(50);
 }
-void Drive60RightRoutine(FCH leftDriver, FCH rightDriver){
-    leftDriver.setSpeed(104);
-    rightDriver.setSpeed(20);
-    delay(20);
-    leftDriver.setSpeed(104);
-    rightDriver.setSpeed(80);
-    delay(20);
-    leftDriver.brake();
+void Drive90RightRoutine(void){
+    driverLeft.setSpeed(90);
+    driverRight.setSpeed(100);
+    delay(450);
+    driverLeft.brake();
+    driverRight.brake();
+    delay(50);
+    driverLeft.setSpeed(96);
+    driverRight.setSpeed(97);
+    delay(500);
+    driverLeft.brake();
+    driverRight.brake();
+    delay(50);
 }
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,23 @@
   #include <Servo.h>
 #endif
 
-PID pid(5,0,0);
-FCH driverLeft(2);
-FCH driverRight(16);
+PID pid(1.4,0,1);
+int solid_line_count = 0;
+
 state_machine sm;
-//Obstacle od(26,13,8,9,8,9);
+Obstacle od(26,13,17,9,33,25);
 RobotState robotState;
 Servo frontServo;
 LineArray lineArray(34,35,32,19,23);
+int previous_pos = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  //od.init();
-  driverLeft.setSpeed(104);
-  driverRight.setSpeed(80);
-  frontServo.attach(9);
+  od.init();
+  //driverLeft.setSpeed(MOTOR_BASE_SPEED_LEFT);
+  //driverRight.setSpeed(MOTOR_BASE_SPEED_RIGHT);
+  frontServo.attach(21);
+  frontServo.write(90);
   lineArray.init();
   sm.transition();
   Serial.begin(9600);
@@ -34,22 +36,28 @@ void setup() {
 }
 
 void loop() {
-  line_position = lineArray.readValue();
-  Serial.print("pos ");
-  Serial.println(line_position);
+  /*line_position = lineArray.readValue();
+  //Serial.print("pos ");
+  //Serial.println(line_position);
   double pidOut = pid.Calculate(line_position, millis());
-  driverLeft.setSpeed(180 - MOTOR_BASE_SPEED_LEFT + pidOut);
-  driverRight.setSpeed(MOTOR_BASE_SPEED_RIGHT - pidOut);
+  driverLeft.setSpeed(constrain(MOTOR_BASE_SPEED_LEFT + pidOut, 90, 105));
+  driverRight.setSpeed(constrain(MOTOR_BASE_SPEED_LEFT - pidOut, 90, 105));
   Serial.print("left");
   Serial.println(MOTOR_BASE_SPEED_LEFT + pidOut);
   Serial.print("right");
   Serial.println(180 - MOTOR_BASE_SPEED_LEFT - pidOut);
   Serial.print("PID ");
   Serial.println(pidOut);
+  if (line_position != previous_pos) {
+    driverLeft.brake();
+    driverRight.brake();
+    delay(15);
+  }
+  previous_pos = line_position;*/
   //delay(60);
   //driverLeft.brake();
   //driverRight.brake();
-  delay(10);
+  //delay(10);
   // put your main code here, to run repeatedly:
   /*if (robotState == RobotState::line_following && od.getDistance_front() < 5) {
     sm.transition();
@@ -57,27 +65,112 @@ void loop() {
   robotState = sm.getCurrentState();
   switch (robotState)
   {
-  case RobotState::obstacle_avoidance:
-  if (od.getDistance_front() < 5) {
+  case RobotState::obstacle_avoidance:*/
+  //Serial.println("front distance :");
+ // Serial.println(od.getDistance_front());
+ //START OF WORKING CODE
+  /*if (od.getDistance_front() < 7) {
+    driverLeft.brake();
+    driverRight.brake();
     float distance_left, distance_right;
-    frontServo.write(60);
-    distance_left = od.getDistance_front();
-    frontServo.write(120);
+    frontServo.write(50);
+    delay(200);
     distance_right = od.getDistance_front();
+    frontServo.write(130);
+    delay(200);
+    distance_left = od.getDistance_front();
     frontServo.write(90);
     if (distance_left > distance_right) {
-      Drive60LeftRoutine(driverLeft, driverRight);
-      Drive60RightRoutine(driverLeft, driverRight);
+      Serial.println("right obstacle");
+      Drive90LeftRoutine();
+      Drive90RightRoutine();
+      Drive90RightRoutine();
 
+    }else{
+      Serial.println("left obstacle");
+      Drive90RightRoutine();
+      Drive90LeftRoutine();
+      Drive90LeftRoutine();
     }
-  }else{
+  }else{*/
     line_position = lineArray.readValue();
-    double pidOut = pid.Calculate(line_position, millis());
-    driverLeft.setSpeed(MOTOR_BASE_SPEED_LEFT + pidOut);
-    driverRight.setSpeed(MOTOR_BASE_SPEED_RIGHT - pidOut);
+    //Serial.print("pos ");
+    //Serial.println(line_position);
+    if (od.getDistance_front() < 10 && od.getDistance_left() < 10 && od.getDistance_right() < 10 && sm.getCurrentState() == RobotState::line_following) {
+      driverLeft.brake();
+      driverRight.brake();
+      delay(3000);
+      driverLeft.setSpeed(80);
+      driverRight.setSpeed(81);
+      delay(200);
+      Drive90LeftRoutine();
+      Drive90LeftRoutine();
+      sm.transition();
+    }else if (sm.getCurrentState() == RobotState::line_following){
+      double pidOut = pid.Calculate(line_position, millis());
+      driverLeft.setSpeed(constrain(MOTOR_BASE_SPEED_LEFT + pidOut, 90, 105));
+      driverRight.setSpeed(constrain(MOTOR_BASE_SPEED_LEFT - pidOut, 90, 105));
+      //Serial.print("left");
+      Serial.println(MOTOR_BASE_SPEED_LEFT + pidOut);
+      //Serial.print("right");
+      Serial.println(180 - MOTOR_BASE_SPEED_LEFT - pidOut);
+      //Serial.print("PID ");
+      Serial.println(pidOut);
+      if (line_position != previous_pos) {
+        driverLeft.brake();
+        driverRight.brake();
+        delay(15);
+      }
+      previous_pos = line_position;
+    }else if (sm.getCurrentState() == RobotState::obstacle_avoidance) {
+      if (line_position == previous_pos) {
+        sm.transition();
+      }
+      if (od.getDistance_front() < 7) {
+        driverLeft.brake();
+        driverRight.brake();
+        float distance_left, distance_right;
+        frontServo.write(50);
+        delay(200);
+        distance_right = od.getDistance_front();
+        frontServo.write(130);
+        delay(200);
+        distance_left = od.getDistance_front();
+        frontServo.write(90);
+        if (distance_left > distance_right) {
+          Serial.println("right obstacle");
+          Drive90LeftRoutine();
+          Drive90RightRoutine();
+          Drive90RightRoutine();
+        }else{
+          Serial.println("left obstacle");
+          Drive90RightRoutine();
+          Drive90LeftRoutine();
+          Drive90LeftRoutine();
+        }
+      }else{
+        double pidOut = pid.Calculate(line_position, millis());
+        driverLeft.setSpeed(constrain(MOTOR_BASE_SPEED_LEFT + pidOut, 90, 105));
+        driverRight.setSpeed(constrain(MOTOR_BASE_SPEED_LEFT - pidOut, 90, 105));
+        //Serial.print("left");
+        Serial.println(MOTOR_BASE_SPEED_LEFT + pidOut);
+        //Serial.print("right");
+        Serial.println(180 - MOTOR_BASE_SPEED_LEFT - pidOut);
+        //Serial.print("PID ");
+        Serial.println(pidOut);
+        if (line_position != previous_pos) {
+          driverLeft.brake();
+          driverRight.brake();
+          delay(15);
+        }
+        previous_pos = line_position;
+      }
+    }
+  
 
-  }
-    break;
+  //}
+  //END OF WORKING CODE
+  /*  break;
   
   default:
     line_position = lineArray.readValue();
