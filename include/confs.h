c2ac8c10cf0682d84bb38146df383722bc21cb24
sammy <sammyoina@gmail.com>
2022-11-15 01:41:15 +0300

    SNAP BACK TO REALITY

--- include/confs.h
+++ include/confs.h
@@ -2,8 +2,8 @@
 #define CONNFS_h
 
 #include <FCH.h>
-#define MOTOR_BASE_SPEED_LEFT 100
-#define MOTOR_BASE_SPEED_RIGHT 100
+#define MOTOR_BASE_SPEED_LEFT 96
+#define MOTOR_BASE_SPEED_RIGHT 96
 
 #define IGNORED_CHECKPOINTS 1
 
@@ -12,24 +12,22 @@ FCH driverRight(16);
 
 int line_position;
 
-void Drive60LeftRoutine(FCH leftDriver, FCH rightDriver)
+void Drive60LeftRoutine(void)
 {
-    leftDriver.setSpeed(20);
-    rightDriver.setSpeed(104);
-    delay(20);
-    leftDriver.setSpeed(104);
-    rightDriver.setSpeed(80);
-    delay(20);
-    leftDriver.brake();
+    driverLeft.setSpeed(80);
+    driverRight.setSpeed(100);
+    delay(300);
+    driverLeft.setSpeed(MOTOR_BASE_SPEED_LEFT);
+    driverRight.setSpeed(MOTOR_BASE_SPEED_RIGHT);
+    delay(200);
 }
-void Drive60RightRoutine(FCH leftDriver, FCH rightDriver)
+void Drive60RightRoutine(void)
 {
-    leftDriver.setSpeed(104);
-    rightDriver.setSpeed(20);
-    delay(20);
-    leftDriver.setSpeed(104);
-    rightDriver.setSpeed(80);
-    delay(20);
-    leftDriver.brake();
+    driverLeft.setSpeed(100);
+    driverRight.setSpeed(80);
+    delay(300);
+    driverLeft.setSpeed(MOTOR_BASE_SPEED_LEFT);
+    driverRight.setSpeed(MOTOR_BASE_SPEED_RIGHT);
+    delay(200);
 }
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@
 #include <obstacle_detector.h>
 #include <confs.h>
 #include <line_array.h>
+#include <maze_solver.h>
 
-PID pid(2.1, 0, 1.1);
+PID pid(1.2, 0, 1.1);
 state_machine sm;
 Obstacle od(26, 13, 22, 17, 33, 25);
 RobotState robotState;
 Servo frontServo;
 LineArray lineArray(34, 35, 32, 19, 23);
+maze_solver mz(RIGHT_WALL, 2,2,2);
 
 int previous_pos = 0;
 int obstacle_count = 0;
@@ -25,24 +27,25 @@ void setup()
   od.init();
   frontServo.attach(21);
   sm.transition();
+  pinMode(LED_BUILTIN, OUTPUT);
   delay(1000);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
   robotState = sm.getCurrentState();
   switch (robotState)
   {
   case RobotState::line_following:
-  {
+  { 
+    digitalWrite(LED_BUILTIN, LOW);
     line_position = lineArray.readValue();
     if (line_position >= 10)
     {
       sm.transition();
       break;
     }
-    if (lineArray.checkpoint_count >= IGNORED_CHECKPOINTS)
+    if (lineArray.checkpoint_count == IGNORED_CHECKPOINTS)
     {
       driverLeft.setSpeed(MOTOR_BASE_SPEED_LEFT);
       driverRight.setSpeed(MOTOR_BASE_SPEED_RIGHT);
@@ -66,23 +69,26 @@ void loop()
   }
   case RobotState::stop_and_go:
   {
+    digitalWrite(LED_BUILTIN, HIGH);
     driverLeft.brake();
     driverRight.brake();
     delay(3000);
     driverLeft.setSpeed(100);
     driverRight.setSpeed(80);
-    delay(1000);
+    delay(300);
     sm.transition();
     break;
   }
 
-  /*default:
-    driverLeft.brake();
-    driverRight.brake();
-    break;*/
+  //default:
+    //driverLeft.brake();
+    //driverRight.brake();
+    //break;
   case RobotState::obstacle_avoidance:
   {
-    if (obstacle_count >= 3 && lineArray.lost_line_count >= 1){
+    digitalWrite(LED_BUILTIN, LOW);
+    if (obstacle_count >= 3 || lineArray.lost_line_count >= 1){
+      //if (obstacle_count >= 3 && lineArray.lost_line_count >= 1){ for actual track
       sm.transition();
       break;
     }
@@ -101,15 +107,15 @@ void loop()
       delay(200);
       if (distance_left > distance_right)
       {
-        Drive60LeftRoutine(driverLeft, driverRight);
-        Drive60RightRoutine(driverLeft, driverRight);
-        Drive60RightRoutine(driverLeft, driverRight);
+        Drive60LeftRoutine();
+        Drive60RightRoutine();
+        Drive60RightRoutine();
       }
       else if (distance_right > distance_left)
       {
-        Drive60RightRoutine(driverLeft, driverRight);
-        Drive60LeftRoutine(driverLeft, driverRight);
-        Drive60LeftRoutine(driverLeft, driverRight);
+        Drive60RightRoutine();
+        Drive60LeftRoutine();
+        Drive60LeftRoutine();
       }
     }
     else
@@ -135,7 +141,37 @@ void loop()
   }
   case RobotState::maze:
   {
-
+    digitalWrite(LED_BUILTIN, HIGH);
+    directions direction = mz.get_turn_directions(od.getDistance_front(), od.getDistance_right(), od.getDistance_left());
+    switch (direction)
+    {
+    case RIGHT:{
+      driverLeft.setSpeed(100);
+      driverRight.setSpeed(80);
+      delay(300);
+      break;
+    }
+    case LEFT:{
+      driverLeft.setSpeed(80);
+      driverRight.setSpeed(100);
+      delay(300);
+      break;
+    }
+    case FOWARD:{
+      driverLeft.setSpeed(MOTOR_BASE_SPEED_LEFT);
+      driverRight.setSpeed(MOTOR_BASE_SPEED_RIGHT);
+      break;
+    }
+    case TURN_AROUND:{
+      driverLeft.setSpeed(80);
+      driverRight.setSpeed(100);
+      delay(400);
+      break;
+    }
+    
+    default:
+      break;
+    }
     break;
   }
 
